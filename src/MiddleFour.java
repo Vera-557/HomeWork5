a6a116b9bbd04d0c228000308eb2df6797be21d2
Daiquiri <mestelo@inbox.ru>
2024-09-22 00:30:13 +0200

    Факториал изменила, проверила на калькуляторе на всякий случй, все правильно

--- src/MiddleFour.java
+++ src/MiddleFour.java
@@ -8,20 +8,13 @@ import java.util.Scanner;
 
 public class MiddleFour {
     public static void main(String[] args) {
-        System.out.println("Введите необходимое количество ячеек массива");
-        int size = new Scanner(System.in).nextInt();
-        System.out.println("Введите числовые значения массива");
-        int[] array = new int[size];
-        for (int element = 0; element < size; element++) {
-            array[element] = new Scanner(System.in).nextInt();
-
+        System.out.println("Введите число, из которого мы вычислим факториал");
+        int num = new Scanner(System.in).nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+            System.out.println("Факториал = " + factorial);
         }
-        int num = 1;
-        for (int element : array) {
-            num *= element;
-
-        }
-        System.out.println("Факториал = " + num);
 
     }
 }
--- src/MiddleSecond.java
+++ src/MiddleSecond.java
@@ -10,14 +10,14 @@ public class MiddleSecond {
         if (1 <= size && size <= 100) {
             double[] massiv = new double[size];
             System.out.println("Введите числовые значения массива");
-            for (int element = 0; element < size; element++) {
+            for (int element = 0; element < size; element++) {//это индекс
                 massiv[element] = new Scanner(System.in).nextDouble();
             }
             double maximum = 0;
-            for (double i : massiv) {
-                int absValue = (int) Math.abs(i);
-                if (absValue > maximum) {
-                    maximum = absValue;
+            for (double i : massiv) {//это элемент
+                int absValue = (int) Math.abs(i);//здесь преобразуем число в абсолют
+                if (absValue > maximum) {//сравниваем с максимумом
+                    maximum = absValue;//перезаписываем
                 }
             }
             System.out.println("Максимальное значение внутри массива = " + maximum);
