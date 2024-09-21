3ffe6c2930635e2001cfcce6b0469acaa6473c30
Daiquiri <mestelo@inbox.ru>
2024-09-22 00:45:55 +0200

    ,

--- src/MiddleFive.java
+++ src/MiddleFive.java
@@ -5,14 +5,17 @@
 
 public class MiddleFive {
     public static void main(String[] args) {
-        int[][] array = new int[][]{{2, 4, 6, 3}, {7, 6, 9, 5}};
+        int[][] array = new int[][]{{2, 4, 6, 3},
+                                    {7, 6, 9, 5}};
         //Кажется я не совсем поняла условие задачи...
-        int sum = array[0][0] + array[1][0];
-        int sum2 = array[0][1] + array[1][1];
-        int sum3 = array[0][2] + array[1][2];
-        int sum4 = array[0][3] + array[1][3];
+        int sum = array[0][0] + array[0][0];
+        int sum2 = array[1][1] + array[1][1];
+        //int sum3 = array[2][2] + array[2][2];
 
-        System.out.println("Вывод = " + sum + "," + sum2 + "," + sum3 + "," + sum4);
+
+
+
+        System.out.println("Вывод = " + sum + "," + sum2 + ","  );
 
 
     }
